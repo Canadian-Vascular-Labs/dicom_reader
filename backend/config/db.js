621fba03e4f44c6ec36efad3ef5698e26e20016d
sanjay-gupta-uw <sanjayguppy@icloud.com>
2025-05-12 13:20:02 -0400

    removed deprecated DB parameters in db.connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,7 @@ const connectDB = async () => {
     try {
         console.log('Connecting to MongoDB with URI:', MONGO_URI);
         await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            bName: process.env.DB_NAME,
+            dbName: process.env.DB_NAME,
 
         });
         console.log('MongoDB connected successfully');
