d51825b45dfe81b7cef29b9bb3f3715080f9c907
sanjay-gupta-uw <sanjayguppy@icloud.com>
2025-05-12 13:17:22 -0400

    added DB name in creation

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,14 @@ const { MONGO_URI } = require('./config')
 const connectDB = async () => {
     try {
         console.log('Connecting to MongoDB with URI:', MONGO_URI);
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bName: process.env.DB_NAME,
+
+        });
         console.log('MongoDB connected successfully');
+        console.log('Connected to DB:', mongoose.connection.db.databaseName);
     } catch (error) {
         console.error('MongoDB connection error:', error);
         process.exit(1); // Exit with failure
